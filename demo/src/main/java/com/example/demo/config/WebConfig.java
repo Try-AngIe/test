3dcc028a543581eee46556bd48087d9503e07da6
김상훈 <rlatkdgns042@naver.com>
2024-07-02 18:30:52 +0900

    test

--- demo/src/main/java/com/example/demo/config/WebConfig.java
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -17,7 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedMethods("*");
 
-
-
     }
 }
--- react/src/Test.jsx
+++ react/src/Test.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable */
 
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Test = () => {
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         axios.get('http://rlatkdalbtest1-1182715068.ap-northeast-2.elb.amazonaws.com/test')
@@ -13,11 +14,42 @@ const Test = () => {
             .catch(err => {
                 console.error('에러임', err);
             });
+        getUser();
     }, []);
+
+    const getUser = () => {
+        axios.get('http://rlatkdalbtest1-1182715068.ap-northeast-2.elb.amazonaws.com/users')
+            .then(res => {
+                setUsers(res.data);
+            })
+            .catch(err => {
+                console.error('에러임', err);
+            });
+    };
+
+    const createUser = () => {
+        const user = { name: 'John Doe', email: 'john.doe@example.com' };
+        axios.post('http://rlatkdalbtest1-1182715068.ap-northeast-2.elb.amazonaws.com/users', user)
+            .then(res => {
+                console.log('[유저 생성]', res.data);
+                getUser();
+            })
+            .catch(err => {
+                console.error('에러임', err);
+            });
+    };
 
     return (
         <>
-            <h1>api 체크</h1>
+            <h1>API 체크</h1>
+            <button onClick={createUser}>테스트 유저 생성</button>
+            <button onClick={getUser}>테스트 유저 조회</button>
+            <h2>Users List</h2>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name} - {user.email}</li>
+                ))}
+            </ul>
         </>
     );
 }
