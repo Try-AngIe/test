5cabca3d9117de71a958f79e621cfed1d3c82a4d
김상훈 <rlatkdgns042@naver.com>
2024-07-03 15:32:19 +0900

    test

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -20,7 +20,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "test v004";
+        return "test v00001";
     }
 
     @PostMapping("/users")
